ac0683c7097b6b549a220fc432d86c362d1a599c
Stephen Reno Lastimosa <reno.lastimosa@gmail.com>
2019-03-20 21:43:01 -0700

    Create main.js
    Broken JS

---
+++ main.js
@@ -0,0 +1,28 @@
+var btn = document.querySelector('button');
+
+
+btn.onclick = function(){
+    alert("hello world")
+}
+
+var wasd = document.querySelector(w)
+
+window.addEventListener("keydown", moveCharacter, false);
+
+function moveCharacter(e){
+    switch(e.keyCode){
+        case 97: alert("left");
+        break;
+        case 38:
+        // up key pressed
+        break;
+    case 39:
+        // right key pressed
+        break;
+    case 40:
+        // down key pressed
+        break;
+    }
+}
+
+funct
